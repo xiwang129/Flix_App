0c6204ab3858881e505925c4b6645a260d57ad1c
xiwang129 <amy.xwang71@gmail.com>
2020-09-20 17:12:48 -0400

    Initial Commit

---
+++ Flix_wang/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix_wang
+//
+//  Created by Xiang W. on 9/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
